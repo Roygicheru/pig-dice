ca8571735237c8be80bf26054d67b27dee6caddb
Roygicheru <gicheru.k.roy@gmail.com>
2019-03-05 19:47:42 +0300

    add prototypes for winning the game and a new game

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,86 @@ function User() {
   this.currentScore = 0;
   this.totalScore = 0;
 }
+
+function diceRoll() {
+  return Math.floor(Math.random() * (4 + 2)) + 1;
+}
+
+User.prototype.newGame = function() {
+  this.score = 0;
+  this.currentScore = 0;
+  this.totalScore = 0;
+}
+
+User.prototype.hold = function() {
+  this.totalScore += this.score;
+  this.currentScore = 0;
+  alert("Next player's turn!")
+}
+
+User.prototype.firstRoll = function() {
+  if (this.score === 1) {
+    this.currentScore = 0;
+    alert("Oops!You've rolled a 1...switch to the next player.");
+  } else {
+    this.currentScore += this.score;
+  }
+}
+
+User.prototype.winner = function() {
+  if (this.totalScore >= 100) {
+    alert("Yaay, you've won!")
+  }
+}
+User.prototype.newGame = function() {
+  this.score = 0;
+  this.currentScore = 0;
+  this.totalScore = 0;
+}
+
+User.prototype.hold = function() {
+  this.totalScore += this.score;
+  this.currentScore = 0;
+  alert("Next player's turn!")
+}
+
+User.prototype.firstRoll = function() {
+  if (this.score === 1) {
+    this.currentScore = 0;
+    alert("Oops!You've rolled a 1...switch to the next player.");
+  } else {
+    this.currentScore += this.score;
+  }
+}
+
+User.prototype.winner = function() {
+  if (this.totalScore >= 100) {
+    alert("Yaay, you've won!")
+  }
+}
+User.prototype.newGame = function() {
+  this.score = 0;
+  this.currentScore = 0;
+  this.totalScore = 0;
+}
+
+// User.prototype.hold = function() {
+//   this.totalScore += this.score;
+//   this.currentScore = 0;
+//   alert("Next player's turn!")
+// }
+//
+// User.prototype.firstRoll = function() {
+//   if (this.score === 1) {
+//     this.currentScore = 0;
+//     alert("Oops!You've rolled a 1...switch to the next player.");
+//   } else {
+//     this.currentScore += this.score;
+//   }
+// }
+//
+// User.prototype.winner = function() {
+//   if (this.totalScore >= 100) {
+//     alert("Yaay, you've won!")
+//   }
+// }
